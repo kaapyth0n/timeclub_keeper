20f7712d3d4fc4bd65ef5bd4ecff52f85164c1a5
Nikita Kapitonov <nikita.kapitonov@gmail.com>
2014-05-01 18:53:13 +0000

    script "test" in package.json

--- tests/test.js
+++ tests/test.js
@@ -2,9 +2,9 @@ var request = require('supertest')
   , app = require(__dirname + '/../app')
 
 describe('GET /', function () {
-  it('should contain text "Express"', function (done) {
+  it('should contain text "Чай Фай"', function (done) {
      request(app)
        .get('/')
-       .expect(/Express/, done)
+       .expect(/Чай Фай/, done)
   })
 })
